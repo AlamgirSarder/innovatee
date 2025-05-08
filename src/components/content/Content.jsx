5b3da062949cc14d290427c854410ce47135f9c1
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-05-08 23:36:03 +0600

    content add

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Container from '../layout/Container'
+import Flex from '../layout/Flex'
 
 const Content = () => {
   return (
@@ -7,9 +8,12 @@ const Content = () => {
     <div className='mt-[336px] pb-[180px]'>
      <Container>
             
-            <div>
-                <h1>How simple is it to use our platform?</h1>
-            </div>
+              <Flex className="justify-between items-center px-14">
+              <h1 className='w-[504px] font-bold font-secondary text-[46px] leading-[58px] tracking-[-1px] text-heading_color'>How simple is it to use our platform?</h1>
+                
+                <p className='w-[530px] text-lg text-para_color leading-8 tracking-[-0.25%]'>This Innovate Con guide explores the most popular platforms and walks you through how to use them to grow your business.</p>
+              </Flex>
+      
             
         </Container>
     </div>
