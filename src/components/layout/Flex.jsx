631f5002bf4be415c675906effe902fb9be2c73c
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-05-13 02:09:48 +0600

    pricing final add

--- src/components/layout/Flex.jsx
+++ src/components/layout/Flex.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Flex = ({children,className}) => {
-  return (
-    <div className={`flex ${className}`}>
-        {children}
-    </div>
-  )
-}
+const Flex = ({ children, className = "", style = {} }) => (
+  <div className={`flex ${className}`} style={style}>
+    {children}
+  </div>
+); 
 
 export default Flex
--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,24 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../layout/Container";
 import pricing1 from "../../assets/pricing1.png";
+import pricing2 from "../../assets/pricing2.png";
+import pricing3 from "../../assets/pricing3.png";
 import Flex from "../layout/Flex";
 import { GiCheckMark } from "react-icons/gi";
 
-
-
 const Pricing = () => {
+  const [hovrIndex, setHoverIndex] = useState(null);
+
   const card = [
     {
       img: pricing1,
-      annual_pricing: "20",
+      bgColor: "#E8F2FA",
+      hoverColor: "#FFFFFF",
+      title: "Personal",
+      dollar: 25,
+      annual_pricing: "35",
+      list: [
+        "Calendar View",
+        "Custom Templates",
+        "List view",
+        "Board view",
+        "Assignees & Due Dates",
+      ],
     },
-      {
-      img: pricing1,
+    {
+      img: pricing2,
+      bgColor: "#FFF0EA",
+      hoverColor: "#FFFFFF",
+      title: "Team",
+      dollar: 10,
       annual_pricing: "20",
+      list: [
+        "Timeline Review",
+        "Custom fields",
+        "Custom Templates",
+        "Task dependencies",
+        "Milestones Manage",
+      ],
     },
-      {
-      img: pricing1,
-      annual_pricing: "20",
+    {
+      img: pricing3,
+      bgColor: "#ECE8F9",
+      hoverColor: "#FFFFFF",
+      title: "Enterprise",
+      dollar: 50,
+      annual_pricing: "40",
+      list: [
+        "Data export & deletion",
+        "Block native integrations",
+        "Custom branding",
+        "Lock custom fields",
+        "Priority support",
+      ],
     },
   ];
 
@@ -30,71 +65,59 @@ const Pricing = () => {
             Pricing Designed To Fit Your Business
           </h2>
 
-          <p className="font-bold font-secondary text-[24px] leading-[100%] text-primary opacity-80 text-center mb-[77px] mt-[39px]">Monthly</p>
+          <p className="font-bold font-secondary text-[24px] leading-[100%] text-primary opacity-80 text-center mb-[77px] mt-[39px]">
+            Monthly
+          </p>
 
           <Flex className="justify-between">
-            {card.map((items) => (
-              <div className="w-[450px] py-10 px-[60px] rounded-[20px] border border-solid border-[#DCE7FE]">
-                <Flex className="w-[90px] h-[90px] rounded-full bg-[#E8F2FA] justify-center items-center mx-auto">
-                  <img src={items.img} alt="#card_img" />
+            {card.map((items, index) => (
+              <div
+                key={index}
+                onMouseEnter={() => setHoverIndex(index)}
+                onMouseLeave={() => setHoverIndex(null)}
+                className="w-[450px] py-10 px-[60px] rounded-[20px] border border-solid border-[#DCE7FE] group hover:bg-primary transition-all duration-[0.5s] cursor-pointer"
+              >
+                <Flex
+                  className="w-[90px] h-[90px] rounded-full justify-center items-center mx-auto"
+                  style={{
+                    backgroundColor:
+                      hovrIndex === index ? items.hoverColor : items.bgColor,
+                  }}
+                >
+                  <img src={items.img} alt={`${items.title} pricing icon`} />
                 </Flex>
-                <h2 className="font-bold font-secondary text-[24px] leading-[36px] text-primary mt-8 text-center">
-                  Personal
+
+                <h2 className="font-bold font-secondary text-[24px] leading-[36px] text-primary mt-8 text-center group-hover:text-[#43E7DF]">
+                  {items.title}
                 </h2>
-                <h3 className="font-secondary font-bold text-[44px] leading-[54px] tracking-[-2%] mb-[14px] text-center">
-                  $10/mth
+                <h3 className="font-secondary font-bold text-[44px] leading-[54px] tracking-[-2%] mb-[14px] text-center group-hover:text-white">
+                  ${items.dollar}/mth
                 </h3>
-                <p className="font-primary font-medium text-lg leading-[18px] text-[#645E76] text-center mb-10">Annual pricing <span className="text-primary">(save {items.annual_pricing}%)</span></p>
+                <p className="font-primary font-medium text-lg leading-[18px] text-[#645E76] text-center mb-10 group-hover:text-white">
+                  Annual pricing{" "}
+                  <span className="text-primary group-hover:text-[#43E7DF]">
+                    (save {items.annual_pricing}%)
+                  </span>
+                </p>
 
                 <ul className="flex flex-col gap-y-[26px]">
+                  {items.list.map((item) => (
                     <li>
-                        <Flex className="gap-x-[22px] items-center">
-                            <div className="w-6 h-6 rounded-full bg-[#F4EBFF] flex justify-center items-center">
-                                <GiCheckMark className="w-[10px] h-[10px] text-primary"/>
-                            </div>
-                            <p className="font-primary font-medium text-para_color text-lg leading-8">Calendar View</p>
-                        </Flex>
+                      <Flex className="gap-x-[22px] items-center">
+                        <div className="w-6 h-6 rounded-full bg-[#F4EBFF] flex justify-center items-center">
+                          <GiCheckMark className="w-[10px] h-[10px] text-primary" />
+                        </div>
+                        <p className="font-primary font-medium text-para_color text-lg leading-8 group-hover:text-white">
+                          {item}
+                        </p>
+                      </Flex>
                     </li>
-                       <li>
-                        <Flex className="gap-x-[22px] items-center">
-                           <div className="w-6 h-6 rounded-full bg-[#F4EBFF] flex justify-center items-center">
-                                <GiCheckMark className="w-[10px] h-[10px] text-primary"/>
-                            </div>
-                            <p className="font-primary font-medium text-para_color text-lg leading-8">Custom Templates</p>
-                        </Flex>
-                    </li>
-                       <li>
-                        <Flex className="gap-x-[22px] items-center">
-                            <div className="w-6 h-6 rounded-full bg-[#F4EBFF] flex justify-center items-center">
-                                <GiCheckMark className="w-[10px] h-[10px] text-primary"/>
-                            </div>
-                            <p className="font-primary font-medium text-para_color text-lg leading-8">List view</p>
-                        </Flex>
-                    </li>
-                       <li>
-                        <Flex className="gap-x-[22px] items-center">
-                           <div className="w-6 h-6 rounded-full bg-[#F4EBFF] flex justify-center items-center">
-                                <GiCheckMark className="w-[10px] h-[10px] text-primary"/>
-                            </div>
-                            <p className="font-primary font-medium text-para_color text-lg leading-8">Board view</p>
-                        </Flex>
-                    </li>
-                       <li>
-                        <Flex className="gap-x-[22px] items-center">
-                           <div className="w-6 h-6 rounded-full bg-[#F4EBFF] flex justify-center items-center">
-                                <GiCheckMark className="w-[10px] h-[10px] text-primary"/>
-                            </div>
-                            <p className="font-primary font-medium text-para_color text-lg leading-8">Assignees & Due Dates</p>
-                        </Flex>
-                    </li>
-                      
+                  ))}
                 </ul>
 
-                <a className="font-primary text-lg font-medium py-5 px-12 bg-primary rounded-md text-white inline-block mt-10 w-full text-center">Purchase Now</a>
-
-
-
-
+                <a className=" group-hover:text-[#2C2643] group-hover:bg-[#43E7DF] font-primary text-lg font-medium py-5 px-12 bg-primary rounded-md text-white inline-block mt-10 w-full text-center">
+                  Purchase Now
+                </a>
               </div>
             ))}
           </Flex>
