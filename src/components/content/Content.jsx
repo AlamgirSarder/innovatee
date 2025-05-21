f317d0a16900e7e3abc86d2215fb9ef70976457d
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-05-22 00:21:31 +0600

    responsive Content part

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -28,27 +28,27 @@ const Content = () => {
   ];
 
   return (
-    <div className="mt-[336px] pb-[160px]">
+    <div className="mt-[50px] md:mt-[336px] pb-[50px] md:pb-[160px]">
       <Container>
-        <Flex className="justify-between items-center px-14">
-          <h1 className="w-[504px] font-bold font-secondary text-[46px] leading-[58px] tracking-[-1px] text-heading_color">
+        <Flex className="justify-between items-center  md:px-14 flex-col md:flex-row">
+          <h1 className="md:w-[504px] font-bold font-secondary text-[36px] md:text-[46px] md:leading-[58px] tracking-[-1px] text-heading_color text-center md:text-left">
             How simple is it to use our platform?
           </h1>
 
-          <p className="w-[530px] text-lg text-para_color leading-8 tracking-[-0.25%]">
+          <p className="md:w-[530px]  text-[14px] md:text-lg text-para_color md:leading-8 tracking-[-0.25%] text-center md:text-left">
             This Innovate Con guide explores the most popular platforms and
             walks you through how to use them to grow your business.
           </p>
         </Flex>
 
-        <Flex className="justify-between px-14 mt-[116px]">
-          {contentdetails.map((item) => (
-            <div className="w-[300px] text-center">
+        <Flex className="justify-between md:px-14 mt-[50px] md:mt-[116px] flex-col md:flex-row">
+          {contentdetails.map((item,index) => (
+            <div key={index} className="md:w-[300px] text-center mt-[20px] md:mt-0">
               <img className="mx-auto" src={item.imgg} alt="#content_icon" />
-              <h2 className="font-secondary text-[24px] text-heading_color leading-8 font-bold mt-[26px] mb-[12px]">
+              <h2 className="font-secondary text-[30px] md:text-[24px] text-heading_color leading-8 font-bold mt-[26px] mb-[12px]">
                 {item.title}
               </h2>
-              <p className="font-primary text-lg text-para_color leading-8">
+              <p className="font-primary text-lg text-para_color leading-[25px] md:leading-8">
                 {item.discription}
               </p>
             </div>
