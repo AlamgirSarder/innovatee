eeabb59abeb8fab30326a35f6f978bc6ad4c12fa
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-05-22 16:20:01 +0600

    features complete

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -23,39 +23,39 @@ const Features = () => {
 
             <Flex className="mt-16 justify-between flex-col md:flex-row">
               <div>
-               <div className="">
-
-                 <img src={feature1} alt="#feature1_img" />
-               </div>
+              
+                 <img src={feature1} alt="#feature1_img" className="m-auto md:m-0"/>
+       
                 <h2 className="font-secondary text-[22px] font-bold leading-[34px] tracking-[-0.5p%] text-heading_color mt-[14px] mb-[6px] text-center md:text-left">
                   Fast and Easy to use
                 </h2>
-                <p className="font-primary text-lg leading-8 text-para_color w-[220px]">
+                <p className="font-primary text-lg leading-8 text-para_color md:w-[220px] text-center md:text-left">
                   Easily to convert API with just a few clicks
                 </p>
               </div>
-              <div>
-                <img src={feature2} alt="#feature1_img" />
-                <h2 className="font-secondary text-[22px] font-bold leading-[34px] tracking-[-0.5p%] text-heading_color mt-[14px] mb-[6px]">
+              <div className="mt-[50px] md:mt-0">
+                <img src={feature2} alt="#feature1_img" className="m-auto md:m-0" />
+                <h2 className="font-secondary text-[22px] font-bold leading-[34px] tracking-[-0.5p%] text-heading_color mt-[14px] mb-[6px] text-center md:text-left">
                   Safely Security
                 </h2>
-                <p className="font-primary text-lg leading-8 text-para_color w-[220px]">
+                <p className="font-primary text-lg leading-8 text-para_color md:w-[220px] text-center md:text-left">
                   All customer data is encrypted
                 </p>
               </div>
             </Flex>
 
-            <Flex className="mt-10  justify-between">
-              <div>
-                <img src={feature3} alt="#feature1_img" />
-                <h2 className="font-secondary text-[22px] font-bold leading-[34px] tracking-[-0.5p%] text-heading_color mt-[14px] mb-[6px]">
+            <Flex className="mt-10 flex-col md:flex-row">
+              <div className="md:w-[220px]">
+                <img src={feature3} alt="#feature1_img" className="m-auto md:m-0"/>
+
+                <h2 className="font-secondary text-[22px] font-bold leading-[34px] tracking-[-0.5p%] text-heading_color mt-[14px] mb-[6px] text-center md:text-left">
                   Get Organized
                 </h2>
-                <p className="font-primary text-lg leading-8 text-para_color w-[220px]">
+                <p className="font-primary text-lg leading-8 text-para_color md:w-[220px] text-center md:text-left">
                   From lists to boards, organize work your way.
                 </p>
               </div>
-              <div className="flex items-end">
+              <div className="flex items-end md:ml-[100px] justify-center mt-[30px] md:mt-0">
                 <a className="bg-primary font-primary text-lg font-medium py-5 px-12 rounded-md text-white">
                   Get Started
                 </a>
