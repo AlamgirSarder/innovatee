b6855f3fb4a7bb95928e811008af5b438644575f
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-05-23 03:20:43 +0600

    intergration

--- src/components/intergration/Intergration.jsx
+++ src/components/intergration/Intergration.jsx
@@ -21,55 +21,56 @@ const Intergration = () => {
                     <p className='text-[#FF7364] font-primary font-medium text-[20px]'>Integrations</p>
                     <h2 className='md:w-[587px] font-secondary text-[30px] md:text-[46px] leading-[40px] md:leading-[58px] tracking-[-1px] text-heading_color font-bold mt-1 mb-5'>Easily integrate with your favorite apps</h2>
                     <p className='md:w-[533px] font-primary text-[16px] md:text-lg leading-[26px] md:leading-8 text-para_color tracking-[-0.25%] mb-10 text-justify md:text-start'>App integration, in a general sense, is the process of bringing resources or capabilities from one application to another. As the world of apps continues to evolve, app integration is becoming expected in many contexts.</p>
-                    <a className="bg-primary font-primary text-lg font-medium py-5 px-12 rounded-md text-white inline-block mb-[50px]">
+                    <a className="bg-primary font-primary text-lg font-medium py-5 px-12 rounded-md text-white inline-block mb-[50px] md:mb-0">
                   Get Started
                 </a>
-                <img className="absolute left-[-142px] bottom-[-159px] w-[152px] h-[152px]" src={intergration_bg} alt="#intergration_bg img" />
+                <img className="hidden md:block absolute left-[-142px] bottom-[-159px] w-[152px] h-[152px]" src={intergration_bg} alt="#intergration_bg img" />
                    
                 </div>
                 
-                <div>
+                <div className='w-full md:w-auto'>
 
-                    <Flex className="gap-x-[30px] flex-col md:flex-row">
+                    <Flex className="gap-x-[20px] md:gap-x-[30px] justify-center md:justify-between ">
 
-                    <div className='flex justify-center items-center w-[332px] h-[142px] rounded-[12px] bg-white m-auto'>
-                        <img src={frame1} alt="#frame1" />
+                    <div className='flex justify-center items-center w-[332px] h-[100px] md:h-[142px] rounded-[12px] bg-white'>
+                        <img src={frame1} alt="#frame1" className='px-[20px] md:px-0'/>
                     </div>
 
                      
-                    <div className=' flex justify-center items-center w-[332px] h-[142px] rounded-[12px] bg-white m-auto mt-[30px] md:mt-0'>
-                        <img src={frame2} alt="#frame2" />
+                    <div className='flex justify-center items-center w-[332px] h-[100px] md:h-[142px] rounded-[12px] bg-white'>
+                        <img src={frame2} alt="#frame2" className='px-[20px] md:px-0' />
                     </div>
                     
                     </Flex>
 
-                    <Flex className="md:mt-[35px] gap-x-[30px] flex-col md:flex-row">
-                    <div className=' flex justify-center items-center w-[151px] h-[142px] rounded-[12px] bg-white m-auto mt-[30px] md:mt-0'>
-                        <img src={frame3} alt="#frame3" />
+                    <Flex className="mt-[20px] md:mt-[35px] gap-x-[20px] md:gap-x-[30px] justify-center md:justify-between ">
+                    <div className=' flex justify-center items-center w-[151px] h-[100px] md:h-[142px] rounded-[12px] bg-white'>
+                        <img src={frame3} alt="#frame3" className='px-[20px] md:px-0' />
                     </div>
 
-                    <div className=' flex justify-center items-center w-[332px] h-[142px] rounded-[12px] bg-white m-auto mt-[30px] md:mt-0'>
-                        <img src={frame4} alt="#frame4" />
+                    <div className=' flex justify-center items-center w-[332px] h-[100px] md:h-[142px] rounded-[12px] bg-white'>
+                        <img src={frame4} alt="#frame4" className='px-[20px] md:px-0'/>
                     </div>
 
-                    <div className=' flex justify-center items-center w-[151px] h-[142px] rounded-[12px] bg-white m-auto mt-[30px] md:mt-0'>
-                        <img src={frame5} alt="#frame5" />
+                    <div className=' flex justify-center items-center w-[151px] h-[100px] md:h-[142px] rounded-[12px] bg-white'>
+                        <img src={frame5} alt="#frame5" className='px-[20px] md:px-0' />
                     </div>
                     </Flex>
 
-                    <Flex className="md:mt-[35px] gap-x-[30px] flex-col md:flex-row">
-                    <div className=' flex justify-center items-center w-[290px] h-[142px] rounded-[12px] bg-white m-auto mt-[30px] md:mt-0'>
-                        <img src={frame6} alt="#frame6" />
+                    <Flex className="mt-[20px] md:mt-[35px] gap-x-[20px] md:gap-x-[30px] justify-center md:justify-between">
+
+                    <div className=' flex justify-center items-center w-[290px] h-[100px] md:h-[142px] rounded-[12px] bg-white'>
+                        <img src={frame6} alt="#frame6" className='px-[20px] md:px-0' />
                     </div>
 
                     
-                    <div className=' flex justify-center items-center w-[151px] h-[142px] rounded-[12px] bg-white m-auto mt-[30px] md:mt-0'>
-                        <img src={frame5} alt="#frame5" />
+                    <div className=' flex justify-center items-center w-[151px] h-[100px] md:h-[142px] rounded-[12px] bg-white'>
+                        <img src={frame5} alt="#frame5"  className='px-[20px] md:px-0'/>
                     </div>
 
                     
-                    <div className=' flex justify-center items-center w-[196px] h-[142px] rounded-[12px] bg-white m-auto mt-[30px] md:mt-0 '>
-                        <img src={frame7} alt="#frame7" />
+                    <div className=' flex justify-center items-center w-[196px] h-[100px] md:h-[142px] rounded-[12px] bg-white'>
+                        <img src={frame7} alt="#frame7"  className='px-[20px] md:px-0'/>
                     </div>
                     </Flex>
 
